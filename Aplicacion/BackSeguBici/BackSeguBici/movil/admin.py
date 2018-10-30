53037c40b646c3c76651909cd83b3dfd0542beac
AndersonJimenez <asjimenez@asesoftware.com>
2018-10-29 19:34:10 -0500

    Se agrega configuracion para modificar tablas desde administrador

--- Aplicacion/BackSeguBici/BackSeguBici/movil/admin.py
+++ Aplicacion/BackSeguBici/BackSeguBici/movil/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Bicicleta)
+admin.site.register(Imagen)
+admin.site.register(Ciudad)
+admin.site.register(Tipoidentificacion)
+admin.site.register(Tipocalle)
+admin.site.register(Cardinalidad)
+admin.site.register(Departamento)
+admin.site.register(Localizacion)
+admin.site.register(Propietario)
+admin.site.register(Geolocalizacion)
 
 # Register your models here.
