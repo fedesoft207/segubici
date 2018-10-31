47a6926c8fb4381574bae6d9261d991c1001db0f
RASP0905 <ronal.sanchez@fuac.edu.co>
2018-10-30 19:48:11 -0500

    se agrega tomar foto y se prueba en un celular

--- Aplicacion/FrontSeguBici/segubicifront/src/pages/registrarse/registrarse.ts
+++ Aplicacion/FrontSeguBici/segubicifront/src/pages/registrarse/registrarse.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the RegistrarsePage page.
@@ -15,11 +16,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegistrarsePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  options: CameraOptions = {
+    quality: 70,
+    targetWidth: 500,
+    targetHeight: 500,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+
+  imagen: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegistrarsePage');
+  }
+
+  tomarFoto() {
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      this.imagen = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
   }
 
 }
