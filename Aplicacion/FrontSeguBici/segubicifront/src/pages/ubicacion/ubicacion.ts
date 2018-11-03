d8f15d429d4d97cbf835dba259fb54b70de342af
RASP0905 <ronal.sanchez@fuac.edu.co>
2018-11-02 21:25:05 -0500

    se genera el mapa de ubicacion, aunque falta probarlo en un celular

--- Aplicacion/FrontSeguBici/segubicifront/src/pages/ubicacion/ubicacion.ts
+++ Aplicacion/FrontSeguBici/segubicifront/src/pages/ubicacion/ubicacion.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { HomePage } from '../home/home';
+import { GoogleMaps, GoogleMap, GoogleMapOptions } from '@ionic-native/google-maps';
 
 /**
  * Generated class for the UbicacionPage page.
@@ -17,6 +18,8 @@ import { HomePage } from '../home/home';
 })
 export class UbicacionPage {
 
+  map: GoogleMap;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation) {
   }
 
@@ -28,11 +31,36 @@ export class UbicacionPage {
 
   obtenerPosicion() {
     this.geolocation.getCurrentPosition().then((coordenadas) => {
-    console.log(coordenadas);
+      this.loadMap(coordenadas);
     }).catch((error) => {
     console.log('Error getting location', error);
     });
     }
+
+    loadMap(coordenadas) {
+      let mapOptions: GoogleMapOptions = {
+      camera: {
+      target: {
+      lat: coordenadas.coords.latitude,
+      lng: coordenadas.coords.longitude
+      },
+      zoom: 18,
+      tilt: 30
+      }
+      };
+      this.map = GoogleMaps.create('map_canvas', mapOptions);
+      this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: coordenadas.coords.latitude,
+        lng: coordenadas.coords.longitude
+        }
+        });
+        }
+       
+
 
   cerrarSesion(){
       //podemos crear un servicio en rest.ts para eliminar el toke del servidor
